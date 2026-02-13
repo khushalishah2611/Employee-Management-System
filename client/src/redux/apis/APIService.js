d135fd71fbe3c854117e1bc32609fa1e7b591eca
khushalishah2611 <khushalishah2611@gmail.com>
2026-02-13 19:02:35 +0530

    Fix HR signup render warning and API client resolution

--- client/src/redux/apis/APIService.js
+++ client/src/redux/apis/APIService.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
+const rawBaseURL = import.meta.env.VITE_EMPLOYEE_API;
+const fallbackBaseURL = 'http://localhost:5000';
+
+if (!rawBaseURL) {
+  console.warn(`VITE_EMPLOYEE_API is not set. Falling back to ${fallbackBaseURL}`);
+}
+
+const resolvedBaseURL = (rawBaseURL || fallbackBaseURL).replace(/\/$/, '');
+
 export const apiService = axios.create({
-  baseURL: import.meta.env.VITE_EMPLOYEE_API,
+  baseURL: resolvedBaseURL,
   headers: {
     'Content-Type': 'application/json',
   },
