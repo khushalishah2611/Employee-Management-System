2e3cca0f26bddd646a3d53fa41a4a8b59b028640
khushalishah2611 <khushalishah2611@gmail.com>
2026-02-16 11:18:30 +0530

    d

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,9 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import mysql from "mysql2/promise";
+import { PrismaMySQL } from "@prisma/adapter-mysql";
 
-const globalForPrisma = globalThis
+const globalForPrisma = globalThis;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({})
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter: new PrismaMySQL(
+      mysql.createPool(process.env.DATABASE_URL)
+    ),
+  });
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
